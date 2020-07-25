fbedfc3fa50a8ea81883b7199b9610cab13c66ae
Glitch (sigma-hacks-2020) <none>
2020-07-25 16:50:20 +0000

    👆🏀 Updated with Glitch

--- sketch.js
+++ sketch.js
@@ -4,18 +4,28 @@
           color, random, rect, ellipse, stroke, image, loadImage, keyCode,
           collideCircleCircle, text, textSize, mouseX, mouseY, strokeWeight, line, 
           mouseIsPressed, windowWidth, windowHeight, noStroke, UP_ARROW, DOWN_ARROW 
-          LEFT_ARROW, RIGHT_ARROW, backgroundColor, round textAlign CENTER floor loadFont
-          textFont createButton mouseIsPressed break*/
-
- 
+          LEFT_ARROW, RIGHT_ARROW, backgroundColor, round textAlign CENTER LEFT floor \
+          loadFont textFont createButton mouseIsPressed break myDraw*/
 
 let duration, pomTimes, backgroundColor, timeX, timeY;
+let score, time, gameIsOver;
+let mouseEllipse,
+  ball1,
+  ball2,
+  ball3,
+  balls = [];
+
 let timerOver = false,
   timerOn = false,
   timerPaused = false,
   gamePage = false;
+
 let timerFont;
 let startTimerButton, stopTimerButton;
+
+const CIRCLE_SIZE = 20,
+  canvasWidth = 400,
+  canvasLength = 400;
 
 function preload() {
   timerFont = loadFont(
@@ -25,7 +35,7 @@ function preload() {
 
 function setup() {
   // Original set up
-  createCanvas(400, 400);
+  createCanvas(canvasWidth, canvasLength);
   colorMode(HSB, 360, 100, 100);
   noStroke();
 
@@ -35,32 +45,39 @@ function setup() {
 
   timerPaused = false;
 
+  // startTimer setup
   startTimerButton = createButton("  start  ");
   startTimerButton.position(width / 2 - 20, 300);
   startTimerButton.style("background-color", color(177, 83, 27));
   startTimerButton.style("border-color", color(176, 100, 81));
   startTimerButton.style("color", color(176, 100, 81));
 
+  // Stop timer setup
   stopTimerButton = createButton("  stop  ");
   stopTimerButton.position(width / 2 - 20, 330);
   stopTimerButton.style("background-color", color(270, 5, 16));
   stopTimerButton.style("border-color", color(44, 96, 100));
   stopTimerButton.style("color", color(44, 96, 100));
   stopTimerButton.hide();
-  
-  pageOne(); 
+
+  //game 1 setup
+  for (var i = 0; i < 4; i++) {
+    balls.push(new Ball());
+  }
+  gameIsOver = false;
+  score = 0;
+  noStroke();
+
+  pageOne();
 }
 
 function draw() {
   background(backgroundColor);
   // pauseTimer();
-  for (var i = 0; i < 3; i++) {
+  
     pageOne();
     countdown();
     runGame();
-  }
-  pageOne();
-  countdown();
 
   // Starter page
   // setInterval(timer(), 10);
@@ -136,15 +153,15 @@ function countdown() {
     // Status
     fill(18, 89, 100);
     rect(15, 15, width - 30, 100, 15);
-    
+
     // Timer
     fill(174, 100, 100);
     textFont(timerFont);
     textSize(100);
     textAlign(CENTER);
-    
-    timeX = width/2; 
-    timeY = height / 2 + 50
+
+    timeX = width / 2;
+    timeY = height / 2 + 50;
 
     setInterval(timer(), 1000);
   }
@@ -182,9 +199,90 @@ function pageOne() {
 // This function sets up the screen to run the game
 function runGame() {
   if (gamePage == true) {
+    //formatting
     backgroundColor = color(270, 5, 16);
     stopTimerButton.hide();
     startTimerButton.hide();
-    duration = 5*60*100; 
+    textSize(12);
+    textAlign(LEFT);
+    
+    // set up 
+    duration = 5 * 60 * 100;
+    
+    fill(290, 80, 100);
+    mouseEllipse = ellipse(mouseX, mouseY, 20);
+    
+  
+    //Game functions
+    balls.forEach(function(ball) {
+      ball.draw();
+      ball.move();
+      ball.collide();
+    });
+    
+    // Display
+    fill(100);
+    text(`Time remaining: ${time}`, 20, 40);
+    text(`Score: ${score}`, 20, 60);
+
+  }
+}
+
+// BREAK
+// Classes go below this line
+class Ball {
+  constructor() {
+    this.coinX = random(width);
+    this.coinY = random(height);
+    this.radius = CIRCLE_SIZE;
+
+    this.worth = round(random(2, 10));
+
+    this.xVel = this.worth;
+    this.yVel = this.worth;
+
+    this.color = random(360);
+  }
+
+  draw() {
+    fill(this.color, 80, 80);
+    ellipse(this.coinX, this.coinY, this.radius);
+    fill(0);
+    text(this.worth, this.coinX - 4, this.coinY + 3);
+  }
+
+  move() {
+    if (gameIsOver) {
+      return;
+    }
+    this.coinX += this.xVel;
+    this.coinY += this.yVel;
+
+    if (this.coinX > canvasWidth || this.coinX < 0) {
+      this.xVel = -this.xVel;
+    }
+
+    if (this.coinY > canvasLength || this.coinY < 0) {
+      this.yVel = -this.yVel;
+    }
+  }
+
+  collide() {
+    if (gameIsOver) {
+      return;
+    }
+    this.hit = collideCircleCircle(
+      mouseX,
+      mouseY,
+      CIRCLE_SIZE,
+      this.coinX,
+      this.coinY,
+      CIRCLE_SIZE
+    );
+    if (this.hit) {
+      score += 1;
+      this.coinX = random(width);
+      this.coinY = random(height);
+    }
   }
 }
